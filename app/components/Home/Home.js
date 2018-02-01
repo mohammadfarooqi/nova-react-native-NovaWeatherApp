bfaec574ed395b50a79accc5d88ccbb28237fdf2
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-01 17:31:43 -0500

    home complete

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, AsyncStorage, Alert, NetInfo } from 'react-native';
+import { StyleSheet, Text, View, TextInput, AsyncStorage, Alert, NetInfo } from 'react-native';
+import moment from 'moment';
 
 import ListWeatherLocation from '../ListWeatherLocation/ListWeatherLocation';
 
@@ -55,13 +56,26 @@ export default class Home extends Component {
       if (isConnected) {
         storedLocationData = [];
 
-        storedLocationData = await Promise.all(storedLocation.map(city => {
-            // console.log('Stored City: ', city);
+        const temp = await Promise.all(storedLocation.map(city => {
+          // console.log('Stored City: ', city);
 
-            return this.fetchData(city);
+          return this.fetchData(city);
         }));
 
-        // console.log(storedLocationData);
+        const temp2 = await Promise.all(temp.map(cityInfo => this.fetchDetailData(cityInfo.id)));
+
+        storedLocation.map((item, i) => {
+          temp2[i] = this.getThreeDaysAvgData(temp2[i]);
+          
+          // console.log('temp = ', JSON.stringify(temp[i]))
+          // console.log('temp2 = ', JSON.stringify(temp2[i]))            
+          
+          storedLocationData.push({
+            overview: temp[i],
+            detail: temp2[i]
+          });
+        });
+
         this.onSave(storedLocation, storedLocationData);
       }
 
@@ -88,14 +102,6 @@ export default class Home extends Component {
   }
 
   fetchData(city) {
-    // fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&APPID=8a826ef7ef79234d6adc634028501964')
-    //   .then((res) => res.json())
-    //   .then((res) => {
-    //     return console.log(res);
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //   })
     return new Promise(async (res, rej) => {
       try {
         const result = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&APPID=8a826ef7ef79234d6adc634028501964&units=metric');
@@ -107,27 +113,115 @@ export default class Home extends Component {
     });
   }
 
+  fetchDetailData(cityId) {
+    return new Promise(async (res, rej) => {
+      try {
+        //https://api.openweathermap.org/data/2.5/forecast?id=6167865&APPID=8a826ef7ef79234d6adc634028501964&units=metric
+        const result = await fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + cityId + '&APPID=8a826ef7ef79234d6adc634028501964&units=metric');
+        res(result.json());
+      } catch (error) {
+        console.log('fetch detail Data error: ', JSON.stringify(error));
+        rej(error);
+      }
+    });
+  }
+
+  getThreeDaysAvgData(obj) {
+    const tomorrow = moment().utc().milliseconds(0).seconds(0).minutes(0).hours(0).add(1, 'days');
+    const fourDays = moment().utc().milliseconds(0).seconds(0).minutes(0).hours(0).add(4, 'days');
+    
+    let temp = obj.list.filter(item => moment(item.dt_txt).utc().isBetween(tomorrow, fourDays, null, '[)'));
+
+    // console.log('getThreeDaysAvgData arr.length = ', obj.list.length, ' // temp.length = ', temp.length);
+
+    let dayStart = tomorrow;
+    let dayEnd = moment(tomorrow).utc().milliseconds(0).seconds(0).minutes(0).hours(0).add(1, 'days');
+
+    // console.log('dayStart => ', dayStart.toString())
+    // console.log('dayEnd => ', dayEnd.toString())    
+    let result = [];
+    let count = 0;
+    let temperature = 0;
+
+    for (let i = 0; i < temp.length; i++) {
+      if (moment(temp[i].dt_txt).utc().isBetween(dayStart, dayEnd, null, '[)')) {
+        count++;
+        temperature += temp[i].main.temp; 
+        // console.log('hitting here 1')        
+      }
+
+      // check to see if we need to rest values 
+      if (temp[i + 1] && moment(temp[i + 1].dt_txt).utc().isSameOrAfter(dayEnd)) {
+        // console.log('hitting here 2')
+        const calcAvg = ((temperature + 0.0) / count).toFixed(2);
+        count = 0;
+        temperature = 0;
+
+        // temp[i] = {
+        //   temperature: calcAvg,
+        //   forDate: dayStart
+        // };
+        result.push({
+          temperature: calcAvg,
+          forDate: dayStart
+        });
+        
+        // reset values
+        dayStart = moment(dayStart).utc().milliseconds(0).seconds(0).minutes(0).hours(0).add(1, 'days');
+        // console.log('resseting start => ', dayStart.toString());
+        dayEnd = moment(dayStart).utc().milliseconds(0).seconds(0).minutes(0).hours(0).add(1, 'days');
+        // console.log('resseting end => ', dayEnd.toString());        
+      } else if (i === temp.length - 1) {
+        // console.log('hitting here 3')        
+        const calcAvg = ((temperature + 0.0) / count).toFixed(2);
+        // temp[i] = {
+        //   temperature: calcAvg,
+        //   forDate: dayStart
+        // };
+        result.push({
+          temperature: calcAvg,
+          forDate: dayStart
+        });
+      }
+    }
+
+    obj.list = result;
+
+    // console.log('returning obj from getThreeDaysData ', JSON.stringify(result));
+
+    return obj;
+  }
+
   async addLocationToList(event) {
     const location = event.nativeEvent.text;
     console.log('Adding Location: ', location);
 
     const locations = [...this.state.locations];
     locations.push(location);
-
     
     let locationData;
     let locationsData;
+    let detailLocationsData;
 
     try {
       locationData = await this.fetchData(location);
-      
-      console.log('fetched: ', JSON.stringify(locationData));
-      
-      locationsData = [...this.state.locationsData];
-      locationsData.push(locationData);
+      console.log('fetched location data: ', JSON.stringify(locationData));  
+
+      detailLocationsData = await this.fetchDetailData(locationData.id);
+      console.log('fetched detail location data for location.id = ', locationData.id);
     } catch (error) {
       Alert.alert('Error', 'There was an error fetching data forom fetchData');            
     }
+
+    // clear our other days other than 3 days
+    detailLocationsData = this.getThreeDaysAvgData(detailLocationsData);
+
+    locationsData = [...this.state.locationsData];
+    // locationsData.push(locationData);
+    locationsData.push({
+      overview: locationData,
+      detail: detailLocationsData
+    });
 
     this.onSave(locations, locationsData);
 
@@ -153,7 +247,7 @@ export default class Home extends Component {
           style={styles.textInput}
         />
         
-        <ListWeatherLocation locationsData={this.state.locationsData}/>
+        <ListWeatherLocation locationsData={this.state.locationsData} navigator={this.props.navigator}/>
       </View>
     );
   }
@@ -162,7 +256,7 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   textInput: {
     height: 40,
-    marginTop: 20,
+    marginTop: 70,
     marginLeft: 10
   }
 });
