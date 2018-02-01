7a31157232f9244a424e2a3cd08801c7c02ce301
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-01 12:40:13 -0500

    added init comp for detil of location weather

---
+++ app/components/DetailWeatherLocation/DetailWeatherLocation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class DetailWeatherLocation extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Detail Page</Text>
+      </View>
+    );
+  }
+}
