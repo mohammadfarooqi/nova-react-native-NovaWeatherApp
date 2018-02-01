4cf78256e6dd4d0c587f3d8d8966d122dd0cdfb6
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-31 22:22:38 -0500

    added textinput functionality

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+import ListLocationsWeather from '../ListLocationsWeather/ListLocationsWeather';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      locations: []
+    };
+  }
+
+  addLocationToList(event) {
+    const location = event.nativeEvent.text;
+    console.log('Adding Location: ', location);
+
+    const locations = [...this.state.locations];
+    locations.push(location);
+
+    this.setState({
+      locations
+    });
+
+    // console.log('Locations array: ', this.state.locations);
+
+    this.textInput.clear();
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is Home View.</Text>
+        <TextInput 
+          placeholder="Enter a City"
+          onSubmitEditing={(e) => this.addLocationToList(e)}
+          ref={input => { this.textInput = input }}
+          style={styles.textInput}
+        />
+        
+        <ListLocationsWeather locations={this.state.locations} />
       </View>
     );
   }
@@ -17,4 +50,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  textInput: {
+    height: 45
+  }
 });
