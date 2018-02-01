c6df34c9180a916098cf7851cb42f5dd2658c59e
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-01 17:31:27 -0500

    detail page complete

--- app/components/DetailWeatherLocation/DetailWeatherLocation.js
+++ app/components/DetailWeatherLocation/DetailWeatherLocation.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import moment from 'moment';
 
 export default class DetailWeatherLocation extends Component {
   render() {
     return (
-      <View>
-        <Text>Detail Page</Text>
+      <View style={styles.container}>
+        {this.props.data.list.map((item, index) => <Text key={index}>{'Date: \t\t\t' + moment(item.forDate).utc().format('MMM DD YYYY') + ' \nTemperature: \t' + item.temperature + '\n'}</Text>)}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 90
+  }
+});
